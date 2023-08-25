baf78bb4d7b0b3de9bf9deae134e7a082ebdfe0b
Vikram Kangotra <vikramkangotra8055@gmail.com>
2023-08-26 01:51:10 +0530

    Pass basic data type to wasm

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 extern "C" {
-    fn alert(s: &str);
+    fn alert(s: i32);
 }
 
 #[no_mangle]
 pub extern "C" fn call_custom_alert() {
     unsafe {
-        alert("Hello");
+        alert(1337);
     }
 }
